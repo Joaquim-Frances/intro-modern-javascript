1c25b5dde495c08c69a1eb77520fff5f17298e8c
Quim <quimifrances@gmail.com>
2021-02-22 13:58:47 +0100

    Import export

---
+++ src/bases/08-Import-Export.js
@@ -0,0 +1,10 @@
+import  heroes  from "./data/heroes";
+
+// const getHeroeByOwner = (owner) => heroes.find(heroe => heroe.owner === owner);
+
+
+const getHeroeByOwner = (owner) => heroes.filter(heroe => heroe.owner === owner);
+
+console.log(getHeroeByOwner('DC'));
+
+
